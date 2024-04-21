f35156641450615958add888098a864c103978e2
minhaj <minhajfathima1023@gmail.com>
2024-04-21 03:46:40 -0400

    TF code

--- userdata.sh
+++ userdata.sh
@@ -3,4 +3,7 @@
 #yum install ansible python3.11-pip.noarch -y | tee -a /opt/userdata.log
 #pip3.11 install botocore boto3 | tee -a /opt/userdata.log
 #yum install ansible -y
-ansible-pull -i localhost, -U https://github.com/devopsmins/expense-ansible expense.yml -e role_name=${role_name} -e env=${env} | tee -a /opt/userdata.log
+#!/bin/bash
+
+ansible-pull -i localhost, -U https://github.com/devopsmins/roboshop-ansible roboshop.yml -e role_name=rabbitmq -e env=${env} | tee -a /opt/userdata.log
+
